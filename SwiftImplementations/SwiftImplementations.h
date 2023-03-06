5acbf0001d5b0cbc644b67fe4b0d5af019bfbb4b
Dave Rahardja <dave@humancode.us>
2023-03-06 09:32:15 -0800

    Initial Commit

---
+++ SwiftImplementations/SwiftImplementations.h
@@ -0,0 +1,18 @@
+//
+//  SwiftImplementations.h
+//  SwiftImplementations
+//
+//  Created by Dave Rahardja on 3/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftImplementations.
+FOUNDATION_EXPORT double SwiftImplementationsVersionNumber;
+
+//! Project version string for SwiftImplementations.
+FOUNDATION_EXPORT const unsigned char SwiftImplementationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftImplementations/PublicHeader.h>
+
+
